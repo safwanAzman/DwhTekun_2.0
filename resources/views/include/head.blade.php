81121a40d5afeaa9f1a0e29dca32ad5d1c32777d
safwan <safwan@csc.net.my>
2021-03-10 15:22:09 +0800

    dashboard

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -64,7 +64,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.22/b-1.6.4/b-flash-1.6.4/b-html5-1.6.4/b-print-1.6.4/datatables.min.js"></script>
-
+    <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
     @stack('js')
 </body>
 </html>
